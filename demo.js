48bc2074da4652c856c1dff4968fc336d2b62084
lisaibei66 <50068263+lisaibei66@users.noreply.github.com>
2019-04-28 15:24:00 +0800

    demo.js

--- demo.js
+++ demo.js
@@ -1,7 +1,4 @@
 {
-"status":true,
-"code":200,
-"data":
 [
 {"id":"1","unique_id":"15033024077728990250","title":"第一条","resource":"http://image.coolapk.com/picture/2017/0821/535189_1503299292_8488.png"}
 ,
